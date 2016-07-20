88470d47a0c2b056f25559418de7e893d1dadd01
gudzdanil <gudz.danil@gmail.com>
2016-07-20 17:16:34 +0300

    sdk

---
+++ script.js
@@ -0,0 +1,20 @@
+var globalSW = null;
+navigator.serviceWorker.register('sw.js', {
+    scope: '.'
+}).then(function (registration) {
+    navigator.serviceWorker.ready.then(function () {
+        globalSW = registration;
+        $(document.body).removeClass('loading');
+        $('.start').on('click', startPushing);
+    });
+});
+
+function startPushing() {
+    if (!globalSW) {
+        return;
+    }
+    for (var i = 0; i < globalSW.tokens.length; i++) {
+        globalSW.triggerPush(null, i);
+        console.log('sending to ' + globalSW.tokens[i]);
+    }
+}
