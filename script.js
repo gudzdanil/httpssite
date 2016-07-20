fea7d3dd82faf0e28554b6c9ce851efad0effe00
gudzdanil <gudz.danil@gmail.com>
2016-07-20 18:18:59 +0300

    sdk

--- script.js
+++ script.js
@@ -4,11 +4,20 @@ var globalSW = null;
 navigator.serviceWorker.register('sw.js').then(function (registration) {
     navigator.serviceWorker.ready.then(function (sw) {
         globalSW = sw;
+        var messageChannel = new MessageChannel();
+        messageChannel.port1.onmessage = function(event) {
+            if (event.data.error) {
+                reject(event.data.error);
+            } else {
+                resolve(event.data);
+            }
+        };
+
         $(document.body).removeClass('loading');
         $('.start').on('click', startPushing);
         event = document.createEvent('Event');
         event.initEvent('push', false, false);
-        navigator.serviceWorker.controller.postMessage('message', 'push');//.dispatchEvent(event);
+        navigator.serviceWorker.controller.postMessage('message', [messageChannel]);//.dispatchEvent(event);
         console.log('sending to ' + 1);
     });
 });
