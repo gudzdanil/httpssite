2d0cfdbe6ed0698a1a9048015334c58ed55ca049
gudzdanil <gudz.danil@gmail.com>
2016-07-20 18:22:51 +0300

    sdk

--- script.js
+++ script.js
@@ -5,6 +5,19 @@ navigator.serviceWorker.register('sw.js').then(function (registration) {
     navigator.serviceWorker.ready.then(function (sw) {
         globalSW = sw;
         var messageChannel = new MessageChannel();
+
+        $(document.body).removeClass('loading');
+        $('.start').on('click', function (){postMessage(123)});
+        // event = document.createEvent('Event');
+        // event.initEvent('push', false, false);
+        // navigator.serviceWorker.controller.postMessage('message', [messageChannel]);//.dispatchEvent(event);
+        console.log('sending to ' + 1);
+    });
+});
+
+function postMessage(message) {
+    return new Promise(function(resolve, reject) {
+        var messageChannel = new MessageChannel();
         messageChannel.port1.onmessage = function(event) {
             if (event.data.error) {
                 reject(event.data.error);
@@ -13,14 +26,10 @@ navigator.serviceWorker.register('sw.js').then(function (registration) {
             }
         };
 
-        $(document.body).removeClass('loading');
-        $('.start').on('click', startPushing);
-        event = document.createEvent('Event');
-        event.initEvent('push', false, false);
-        navigator.serviceWorker.controller.postMessage('message', [messageChannel]);//.dispatchEvent(event);
-        console.log('sending to ' + 1);
+        navigator.serviceWorker.controller.postMessage(message,
+            [messageChannel.port2]);
     });
-});
+}
 
 function startPushing() {
     if (!globalSW) {
